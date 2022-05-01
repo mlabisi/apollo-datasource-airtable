786783d4023fb823c90aaeb5bbfbf6bc32705cde
Mora <mlabisi98@gmail.com>
2022-05-01 13:51:40 -0700

    prettifying

--- src/datasource.js
+++ src/datasource.js
@@ -23,5 +23,5 @@ class AirtableDataSource extends DataSource {
 }
 
 module.exports = {
-  AirtableDataSource
-}
+  AirtableDataSource,
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-module.exports = require('./datasource')
+module.exports = require('./datasource');
